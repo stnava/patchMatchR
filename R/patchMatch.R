d7494b66258af0dfae6339feb5c4e54a3bf27fc0
stnava <stnava@gmail.com>
2019-08-01 16:23:23 -0400

    BUG: scaling the weights

--- R/patchMatch.R
+++ R/patchMatch.R
@@ -533,12 +533,12 @@ deepFeatures <- function( x, mask, patchSize = 64 ) {
     vgg3Dweights = get_weights( vggmodel )
     vgg2Dweights = get_weights( vggmodel2D )
     for ( j in 1:nchan )
-      vgg3Dweights[[1]][ , , , j , ] = vgg2Dweights[[1]]
+      vgg3Dweights[[1]][ , , , j , ] = vgg2Dweights[[1]] / nchan
     for ( k in c( 2, 4, 6, 8 ) )
       vgg3Dweights[[k]] = vgg2Dweights[[k]]
     for ( k in c( 5, 7 ) )
       for ( j in 1:nchan )
-        vgg3Dweights[[k]][ , , j, , ] = vgg2Dweights[[k]]
+        vgg3Dweights[[k]][ , , j, , ] = vgg2Dweights[[k]] / nchan
     set_weights( vggmodel, vgg3Dweights )
     }
 
@@ -558,4 +558,98 @@ deepFeatures <- function( x, mask, patchSize = 64 ) {
   vecdim = prod( dim( features )[-1]  )
   features = as.matrix( array( features,  dim = c( nrow( features), vecdim ) ) )
   return( list( features=features, patches=patches0, patchCoords = patchCoords ) )
+}
+
+
+#' Random sample consensus (RANSAC)
+#'
+#' @param data dataframe with target column and model matrix columns
+#' @param y_col name of target column
+#' @param model model function to fit
+#' @param model_args list, arguments to the model
+#' @param n the minimum number of data values required to fit the model
+#' @param k the maximum number of iterations allowed in the algorithm
+#' @param t a threshold value for determining when a data point fits a model
+#' @param d the number of close data values required to assert that a model fits well to data
+#'
+#' @return a list of output contains best fitted model, inliers, outliers
+#'
+RANSAC <- function(data, y_col, model, model_args, n = 2, k = NA, t = NA, d = NA, verbose = FALSE) {
+
+  default_ratio = 0.7
+
+  model_args_all <- c(model_args, data = list(data))
+  model_all <- do.call(model, args = model_args_all)
+  err_abs <- abs(data[, y_col] - predict(model_all, newdata = data[, names(df) != y_col, drop=FALSE]))
+
+  if(is.na(t)) {
+    t <- quantile(err_abs, 0.5, names = FALSE)
+  }
+  if(is.na(d)) {
+    d <- nrow(data)*default_ratio
+  }
+  if(is.na(k)) {
+    k <- as.integer(log(1 - 0.99) / log(1 - default_ratio^n))
+  }
+
+  mae_best <- mean(err_abs)
+  ind <- 1:nrow(data)
+  inliers <- c()
+  i = 1
+
+  if(verbose) {
+    progress_pct = round(quantile(seq(1, k, length.out = 10), probs = seq(0.1, 1, length.out = 10)), 0)
+    cat(sprintf("Begin RANSAC algoritm with parameters:\nn = %s\nk = %s\nt = %s \nd = %s\n\n", n, k, t, d))
+  }
+
+  while(i <= k) {
+
+    if(verbose && (i %in% progress_pct) ) {
+      cat(sprintf("Completion percentage: %s\n", names(progress_pct)[which(i == progress_pct)]))
+    }
+
+    inliers_case <- sample(ind, n)
+    model_args_case <- c(model_args, data = list(data[inliers_case, , drop=FALSE]))
+    model_case <- do.call(model, model_args_case)
+
+    predict_args_case = list(
+      object = model_case,
+      newdata = data[-inliers_case, , drop=FALSE]
+    )
+    yhat <- do.call(predict, predict_args_case)
+    y <- data[-inliers_case, y_col]
+    res <- abs(yhat - y)
+
+    inliers_case <- c(inliers_case, names(res)[which(res < t)])
+    if(length(inliers_case) > d) {
+      data_inliers <- data[inliers_case, , drop=FALSE]
+      model_inliers_args <- c(model_args, data = list(data_inliers))
+      model_inliers <- do.call(model, model_inliers_args)
+
+      predict_inliers_args_case = list(
+        object = model_inliers,
+        newdata = data_inliers
+      )
+      mae_inliers <- mean(abs(data_inliers[, y_col] - do.call(predict, predict_inliers_args_case)))
+      if(mae_inliers < mae_best) {
+        model_best <- model_inliers
+        mae_best <- mae_inliers
+        inliers <- inliers_case
+      }
+    }
+    i = i + 1
+  }
+
+  if(!exists("model_best")) {
+    warning("Final model could not be found, using all data to fit the model. ")
+    model_best <- model_all
+  }
+  model_best["call"] <- sprintf("Model with formula: %s", model_args[["formula"]])
+
+  inliers_df = data[sort(inliers), , drop = FALSE]
+  outliers_df = data[setdiff(ind, sort(inliers)), , drop = FALSE]
+  if(verbose) {
+    cat(sprintf("%s (%.1f%%) inliers out of %s total data points have been used. \n", length(inliers), length(inliers) / nrow(data) * 100, nrow(data)))
+  }
+  return(list(model = model_best, inliers = inliers_df, outliers = outliers_df))
 }
