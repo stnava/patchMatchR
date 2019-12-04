af1daa5c33d8d02eebbfb8e21665f74258008b40
stnava <stnava@gmail.com>
2019-12-04 06:28:59 -0500

    ENH: test example

--- exec/test_fish_silhouette.R
+++ exec/test_fish_silhouette.R
@@ -2,8 +2,9 @@ library( keras )
 library( ANTsR )
 library( patchMatchR )
 layout( matrix(1:2,nrow=1) )
-nP1 = 15
-nP2 = 200
+idim = 2 # image dimensionality
+nP1 = 30
+nP2 = 300
 psz = 32
 img1 <- ri( 1 ) %>% iMath( "Normalize" )
 img2 <- ri( 2 ) %>% iMath( "Normalize" )
@@ -20,11 +21,10 @@ bordMask2 = fullMask2 - iMath(fullMask2,"ME",1)
 mask1 = randomMask( bordMask1, nP1 )
 mask2 = randomMask( bordMask2, nP2 )
 matchO = deepPatchMatch(
-  iMath(bordMask2,"D"), iMath(bordMask1,"D"),
-# smoothImage(img2,0.5), smoothImage(img1,0.5),
+#  iMath(bordMask2,"D"), iMath(bordMask1,"D"),
+ smoothImage(img2,0.5), smoothImage(img1,0.5),
  mask2, mask1, block_name = 'block2_conv2',  knn = 1, knnSpatial=50 )
 mlm = matchedLandmarks( matchO, img1, img2, rep(psz, idim) )
-idim = img1@dimension
 k = 1:nrow( mlm$fixedPoints)
 lmImage1 = makePointsImage(
   matrix(mlm$fixedPoints[k,],ncol=idim), img1, radius = 2 )
