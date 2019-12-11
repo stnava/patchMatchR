9c32e2cc04160483ab0a5375db75fde365829c89
stnava <stnava@gmail.com>
2019-12-11 13:10:11 -0800

    ENH: better params

--- exec/test_r_brain.R
+++ exec/test_r_brain.R
@@ -1,16 +1,18 @@
 library( keras )
 library( ANTsR )
 library( patchMatchR )
+layout( matrix(1:3,nrow=1) )
 idim = 2 # image dimensionality
-nP1 = 30
-nP2 = 300
+nP1 = 25
+nP2 = 3000
 psz = 32
 img1 <- ri( 1 ) %>% iMath( "Normalize" )
 img2 <- ri( 2 ) %>% iMath( "Normalize" )
 img2 = antsRegistration( img1, img2, "Rigid" )$warpedmovout
-scaleParam = 2.0
-fullMask1 = iMath(img1*2000,"Canny",scaleParam,9,10)
-fullMask2 = iMath(img2*2000,"Canny",scaleParam,9,10)
+scaleParam = 2.5
+fullMask1 = iMath(img1*2000,"Canny",scaleParam,8,10)
+plot( fullMask1 )
+fullMask2 = iMath(img2*2000,"Canny",scaleParam,8,10)
 mask1 = randomMask( fullMask1, nP1 )
 mask2 = randomMask( fullMask2, nP2 )
 matchO = deepPatchMatch(
@@ -22,6 +24,5 @@ lmImage1 = makePointsImage(
   matrix(mlm$fixedPoints[k,],ncol=idim), img1, radius = 2 )
 lmImage2 = makePointsImage(
   matrix(mlm$movingPoints[k,],ncol=idim), img2, radius = 2 )
-layout( matrix(1:2,nrow=1) )
 plot( img1*222, lmImage1, doCropping=T  )
 plot( img2*222, lmImage2, doCropping=T   )
